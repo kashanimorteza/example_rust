2ef53073ec817553a1d6bf80062b379fda16f7b1
morteza <kashani.morteza@gmail.com>
2025-07-16 13:36:35 +0330

    ex3

---
+++ codes/ex_03/target/debug/deps/ex_03-8a09a02ee62d4d9a.d
@@ -0,0 +1,5 @@
+/Volumes/data/documents/example_rust/codes/ex_03/target/debug/deps/ex_03-8a09a02ee62d4d9a.d: src/main.rs
+
+/Volumes/data/documents/example_rust/codes/ex_03/target/debug/deps/ex_03-8a09a02ee62d4d9a: src/main.rs
+
+src/main.rs:
---
+++ codes/ex_03/target/debug/ex_03.d
@@ -0,0 +1,1 @@
+/Volumes/data/documents/example_rust/codes/ex_03/target/debug/ex_03: /Volumes/data/documents/example_rust/codes/ex_03/src/main.rs
